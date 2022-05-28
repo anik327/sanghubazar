cf1698639bac45d814b8035cd4800d3d0bd75e32
Anik Das <anikdas.ad98@gmail.com>
2022-05-29 03:26:16 +0600

    first commit

---
+++ playground/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def say_hello(request):
+    x = 1
+    y = 2
+    return render(request, 'hello.html', {'name': 'mosh'})
