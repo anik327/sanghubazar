ab35dcfd27e598364fe38b619b2764f21bab912a
Anik Das <anikdas.ad98@gmail.com>
2022-10-02 20:25:43 +0600

    Added cors header for heroku

--- sanghubazar/settings/common.py
+++ sanghubazar/settings/common.py
@@ -67,6 +67,7 @@ INTERNAL_IPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8001',
     'http://127.0.0.1:8001',
+    'http://sanghubazar-api.herokuapp.com/'
 ]
 
 ROOT_URLCONF = 'sanghubazar.urls'
