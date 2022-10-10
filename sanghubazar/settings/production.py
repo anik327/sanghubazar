ae2f3ccf3535315223caeb697908df564ce818b8
Anik Das <anikdas.ad98@gmail.com>
2022-10-10 20:45:35 +0600

    Debug turned on in production settings

--- sanghubazar/settings/production.py
+++ sanghubazar/settings/production.py
@@ -6,7 +6,7 @@ from .common import *
 SECRET_KEY = 'o)kd02_=w%qqeiv81vhkpl6^!*&txmpi#oy$z4-y+)-vdxc&x8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['sanghubazar-api.herokuapp.com']
