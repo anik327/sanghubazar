e000854655afbee7bf93a62ba117ecdea9daf49c
Anik Das <anikdas.ad98@gmail.com>
2022-10-14 22:44:39 +0600

    Edited settings for production

--- sanghubazar/settings/production.py
+++ sanghubazar/settings/production.py
@@ -1,15 +1,15 @@
 import os
 import dj_database_url
-from sanghubazar.settings.development import SECRET_KEY
 from .common import *
 
-SECRET_KEY = 'o)kd02_=w%qqeiv81vhkpl6^!*&txmpi#oy$z4-y+)-vdxc&x8'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['sanghubazar-api.herokuapp.com']
+ALLOWED_HOSTS = ['https://sanghubazar.ondigitalocean.app']
 
 
 DATABASES = {
