45dc52d956064570d51069476e11e1e1f8d00469
Anik Das <anikdas.ad98@gmail.com>
2022-10-17 20:18:58 +0600

    deleted exception for database env

--- sanghubazar/settings/production.py
+++ sanghubazar/settings/production.py
@@ -11,8 +11,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
-if os.getenv("DATABASE_URL", None) is None:
-    raise Exception("DATABASE_URL environment variable not defined")
+
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
