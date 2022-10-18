081efff7cb5587bc44f49649b843bf6cb73ae3db
Anik Das <anikdas.ad98@gmail.com>
2022-10-19 01:27:49 +0600

    CORS Origin all allowed

--- sanghubazar/settings/common.py
+++ sanghubazar/settings/common.py
@@ -68,11 +68,11 @@ INTERNAL_IPS = [
     "127.0.0.1",
     # ...
 ]
-
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8001',
-    'http://127.0.0.1:8001',
-]
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:8001',
+#     'http://127.0.0.1:8001',
+# ]
 
 ROOT_URLCONF = 'sanghubazar.urls'
 
